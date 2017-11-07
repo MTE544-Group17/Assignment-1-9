94a50ac2afc7128ca6322ec562ef39e603894111
Mihai Tiuca <teodor.tiuca@gmail.com>
2017-11-07 18:56:44 -0500

    Remove commented code

--- q4.m
+++ q4.m
@@ -17,20 +17,7 @@ close all
             0        0         1];
         
     g = [x1; x2; x3] + k*j*[u1; u2; u3];
-    
-%     g = [x1 + (-u1/3 + u2 * cosd(60)/3 + u3 * cosd(60)/3) * cos(x3) - (u2 * sind(-60)/2 + u3 * sind(60)/2) * sin(x3);
-%          x2 + (-u1/3 + u2 * cosd(60)/3 + u3 * cosd(60)/3) * sin(x3) + (u2 * sind(-60)/2 + u3 * sind(60)/2) * cos(x3);
-%          x3 + u1 / (3*l) + u2 / (3*l) + u3 / (3*l)];
-     
     G = jacobian(g,  [x1; x2; x3]);
-    
-%% Question 3 : Measurement Model
-% x = [0 0 0];
-% measurement_model = [x(1); x(2); x(3)-99.7/180*pi];
-% measurement_model_disturbance = [randn(1)*0.5; randn(1)*0.5; randn(1)*10/180*pi];
-H = [1 0 0;
-    0 1 0;
-    0 0 1];
 
 %% Question 4: Extended Kalman Filter
 
@@ -64,18 +51,14 @@ mu_S = zeros(3, length(T));
 
 for t = 1 : length(T)
     % Calculate error and disturbance
-%     e = [randn(1);randn(1);randn(1)] * (sqrt(R) * [1; 1; 1]);
     e = normrnd(0,[R(1,1);R(2,2);R(3,3)]);
-%     d = [randn(1);randn(1);randn(1)] * (sqrt(Q) * [1; 1; 1]);
     d = normrnd(0,[Q(1,1);Q(2,2);Q(3,3)]);
     
     % Calculate motion model and measurement model measurement
     x = motion_model(x, u) + e;
     y = [x(1); x(2); x(3)-9.7/180*pi] + d;
     
-    
     %%% Kalman Filter Prediction
-    
     % Propagate mu through the nonlinear motion model
     mup = motion_model(mu,u);
     
@@ -91,15 +74,8 @@ for t = 1 : length(T)
     % Compute predicted covariance
     Sp = G_t*S*G_t' + R;
     
-    %%% Measurement update
-    % Linearized measurement model at the predicted mean
-%     H_t = [mup(1) 0 0;
-%            0 mup(2) 0;
-%            0 0 mup(3)];
-       
-    H_t = eye(3);
-    
     % Compute Kalman gain
+    H_t = eye(3);
     Kg = Sp*H_t'*inv(H_t*Sp*H_t' + Q);
     
     % Update mean using the nonlinear measurement model
@@ -113,7 +89,6 @@ for t = 1 : length(T)
     % Store results
     mup_S(:,t) = mup;
     mu_S(:,t) = mu;
-%     K_S(:,t) = Kg;
 
     % Plot results
     figure(1);clf; hold on;
